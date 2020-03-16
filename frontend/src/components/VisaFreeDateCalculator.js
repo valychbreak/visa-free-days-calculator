5bfbec52a1a170d98b6de441a3f92aea2109ac2f
valychbreak <valychbreak@gmail.com>
2020-03-16 21:35:24 +0100

    Add form for adding new period

--- frontend/src/components/VisaFreeDateCalculator.js
+++ frontend/src/components/VisaFreeDateCalculator.js
@@ -4,9 +4,20 @@ import Axios from 'axios'
 
 class VisaFreeDateCalculator extends Component {
 
-    state = {
-        travelPeriods: [],
-        failed: false
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            travelPeriods: [],
+            failed: false,
+            startDate: '', 
+            endDate: '', 
+            country: '', 
+            note: ''
+        };
+
+        this.saveNewPeriod = this.saveNewPeriod.bind(this);
+        this.handlePeriodInputChange = this.handlePeriodInputChange.bind(this);
     }
 
     componentDidMount() {
@@ -19,13 +30,53 @@ class VisaFreeDateCalculator extends Component {
             })
     }
 
+    saveNewPeriod(event) {
+        console.log('Created new period');
+        event.preventDefault();
+
+        const newTravelPeriod = {start: this.state.startDate, end: this.state.endDate, country: this.state.country, note: this.state.note}
+        console.log(newTravelPeriod);
+
+        this.setState(state => {
+            const periods = state.travelPeriods.concat(newTravelPeriod);
+
+            return {travelPeriods: periods}
+        });
+    }
+
+    handlePeriodInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
+
+        this.setState({[inputName]: inputValue});
+    }
+
     render() {
         return (
             <div>
                 <h2>Calculator</h2>
+                <h3>New period</h3>
+                <div>
+                    <form onSubmit={this.saveNewPeriod}>
+                        <label>Start</label>
+                        <input type="text" onChange={this.handlePeriodInputChange} name="startDate" />
+
+                        <label>End</label>
+                        <input type="text" onChange={this.handlePeriodInputChange} name="endDate" />
+
+                        <label>Country</label>
+                        <input type="text" onChange={this.handlePeriodInputChange} name="country" />
+
+                        <label>Note</label>
+                        <input type="text" onChange={this.handlePeriodInputChange} name="note" />
+
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
                 <h3>Added periods</h3>
                 <ul>
-                {this.state.travelPeriods.map(period => <li>{period.start} - {period.end}, {period.country}. Note: {period.note}</li>)}
+                {this.state.travelPeriods.map((period, index) => <li key={index}>{period.start} - {period.end}, {period.country}. Note: {period.note}</li>)}
                 </ul>
             </div>
         )
