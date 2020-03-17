9a9f7b64979c223bd31692f5ba46662f67bc6855
valychbreak <valychbreak@gmail.com>
2020-03-17 23:58:52 +0100

    Fix DOM validation issues with table

--- frontend/src/components/calculator/TravelPeriodsList.js
+++ frontend/src/components/calculator/TravelPeriodsList.js
@@ -5,16 +5,20 @@ class TraverlPeriodsList extends Component {
     render() {
         return (
             <table>
-                <tr>
-                    <th>Travel Period</th>
-                    <th>Country</th>
-                    <th>Note</th>
-                </tr>
-                {this.props.travelPeriods.map((period, index) => (
-                    <tr key={index}>
-                        <td>{period.start} - {period.end}</td><td>{period.country}</td><td>{period.note}</td>
+                <thead>
+                    <tr>
+                        <th>Travel Period</th>
+                        <th>Country</th>
+                        <th>Note</th>
                     </tr>
-                ))}
+                </thead>    
+                <tbody>
+                    {this.props.travelPeriods.map((period, index) => (
+                        <tr key={index}>
+                            <td>{period.start} - {period.end}</td><td>{period.country}</td><td>{period.note}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         )
     }
