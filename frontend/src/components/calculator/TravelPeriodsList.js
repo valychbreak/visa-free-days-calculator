b03ab7b3beeefe9e51bfd957695cfce2bd76ad90
valychbreak <valychbreak@gmail.com>
2020-03-20 18:22:05 +0100

    Replace Grid with table for displaying travel periods

--- frontend/src/components/calculator/TravelPeriodsList.js
+++ frontend/src/components/calculator/TravelPeriodsList.js
@@ -1,43 +1,31 @@
 import React, { Component } from 'react'
 import Moment from 'react-moment';
-import { Grid } from '@material-ui/core';
+import { TableContainer, Table, TableRow, TableCell, TableHead, TableBody } from '@material-ui/core';
 
 class TraverlPeriodsList extends Component {
 
     render() {
         return (
-            // <table>
-            //     <thead>
-            //         <tr>
-            //             <th>Travel Period</th>
-            //             <th>Country</th>
-            //             <th>Note</th>
-            //         </tr>
-            //     </thead>    
-            //     <tbody>
-            //         {this.props.travelPeriods.map((period, index) => (
-            //             <tr key={index}>
-            //                 <td><Moment date={period.start} format="DD-MM-YYYY"/> - <Moment date={period.end} format="DD-MM-YYYY"/></td>
-            //                 <td>{period.country}</td>
-            //                 <td>{period.note}</td>
-            //             </tr>
-            //         ))}
-            //     </tbody>
-            // </table>
-            <div>
-                <Grid container >
-                    <Grid item xs={4}>Travel Period</Grid>
-                    <Grid item xs={4}>Country</Grid>
-                    <Grid item xs={4}>Note</Grid>
-                </Grid>
-                {this.props.travelPeriods.map((period, index) => (
-                    <Grid container key={index}>
-                            <Grid item xs={4}><Moment date={period.start} format="DD-MM-YYYY"/> - <Moment date={period.end} format="DD-MM-YYYY"/></Grid>
-                            <Grid item xs={4}>{period.country}</Grid>
-                            <Grid item xs={4}>{period.note}</Grid>
-                    </Grid>
-                ))}
-            </div>
+            <TableContainer >
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Travel Period</TableCell>
+                            <TableCell>Country</TableCell>
+                            <TableCell>Note</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.travelPeriods.map((period, index) => (
+                            <TableRow key={index}>
+                                <TableCell><Moment date={period.start} format="DD-MM-YYYY"/> - <Moment date={period.end} format="DD-MM-YYYY"/></TableCell>
+                                <TableCell>{period.country}</TableCell>
+                                <TableCell>{period.note}</TableCell>
+                            </TableRow>
+                        ))} 
+                    </TableBody>
+                </Table>
+            </TableContainer>
         )
     }
 }
