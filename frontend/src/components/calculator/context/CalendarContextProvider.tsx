f9058ed38e4c3eb30014de7d673ec0711a4e12bc
Valerii Brylov <valychbreak@gmail.com>
2020-03-22 19:12:51 +0100

    Code refactoring

--- frontend/src/components/calculator/context/CalendarContextProvider.tsx
+++ frontend/src/components/calculator/context/CalendarContextProvider.tsx
@@ -16,50 +16,60 @@ class CalculatorContextProvider extends Component<{}, CalculatorProviderState> {
 
     constructor(props: any) {
         super(props);
-        
         this.state = CalculatorContextProvider.DEFAULT_STATE;
     }
 
     componentDidMount() {
-        Axios.get('http://localhost:8080/api/period/all')
-            .then(res => {
-                this.setState({travelPeriods: res.data, failed: false})
-            })
-            .catch(err => {
-                this.setState({travelPeriods: [], failed: true})
-            })
+        this.loadTravelPeriods();
     }
 
     render() {
         return (
             <CalculatorContext.Provider value={{
                 travelPeriods: this.state.travelPeriods,
-                addNewPeriod: (travelPeriod: TravelPeriod) => {
-
-                    const params = new URLSearchParams();
-                    params.append('start', travelPeriod.start.format('YYYY-MM-DD'));
-                    params.append('end', travelPeriod.end.format('YYYY-MM-DD'));
-                    params.append('country', travelPeriod.country);
-                    params.append('note', travelPeriod.note);
-
-                    return Axios.post("http://localhost:8080/api/period/add", params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
-                        .then(response => {
-                            const createTravelPeriod = response.data as TravelPeriod;
-                            this.setState({travelPeriods: [...this.state.travelPeriods, createTravelPeriod]});
-                            return createTravelPeriod;
-                        });
-                },
-                deleteTravelPeriod: (travelPeriod: TravelPeriod) => {
-                    Axios.delete('http://localhost:8080/api/period/' + travelPeriod.id + '/delete')
-                        .then(reponse => {
-                            const newTravelPeriods = this.state.travelPeriods.filter(existingTravelPeriod => existingTravelPeriod.id !== travelPeriod.id);
-                            this.setState({travelPeriods: [...newTravelPeriods]});
-                        });
-                }
+                addNewPeriod: (travelPeriod: TravelPeriod) => this.addTravelPeriod(travelPeriod),
+                deleteTravelPeriod: (travelPeriod: TravelPeriod) => this.deleteTravelPeriod(travelPeriod)
             }}>
                 {this.props.children}
             </CalculatorContext.Provider>
         )
+    }
+
+    private async addTravelPeriod(travelPeriod: TravelPeriod) {
+        const params = new URLSearchParams();
+        params.append('start', travelPeriod.start.format('YYYY-MM-DD'));
+        params.append('end', travelPeriod.end.format('YYYY-MM-DD'));
+        params.append('country', travelPeriod.country);
+        params.append('note', travelPeriod.note);
+
+        const config = {
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        };
+
+        const response = await Axios.post("http://localhost:8080/api/period/add", params, config);
+        if (response === undefined) { throw new Error("test")}
+        const createTravelPeriod = (response.data as TravelPeriod);
+        this.setState({ travelPeriods: [...this.state.travelPeriods, createTravelPeriod] });
+        return createTravelPeriod;
+    }
+
+    private deleteTravelPeriod(travelPeriod: TravelPeriod) {
+        Axios.delete('http://localhost:8080/api/period/' + travelPeriod.id + '/delete')
+            .then(reponse => {
+                const newTravelPeriods = this.state.travelPeriods.filter(existingTravelPeriod => existingTravelPeriod.id !== travelPeriod.id);
+                this.setState({travelPeriods: [...newTravelPeriods]});
+            });
+    }
+
+    private loadTravelPeriods() {
+        Axios.get('http://localhost:8080/api/period/all')
+            .then(res => {
+                this.setState({ travelPeriods: res.data, failed: false });
+            })
+            .catch(err => {
+                this.setState({ travelPeriods: [], failed: true });
+                throw err;
+            });
     }
 }
 
