fd112d539dbe4900cd370b272235cd009526bff8
valychbreak <valychbreak@gmail.com>
2020-03-31 01:38:18 +0200

    Export user context interface

--- frontend/src/components/authentication/context/UserContext.tsx
+++ frontend/src/components/authentication/context/UserContext.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import User from '../../../common/User'
 import AccessToken from '../../../common/AccessToken';
 
-type UserContext = {
+export interface UserContext {
     isAuthenticated(): boolean;
     getUser(): User | null;
     setUser(user: User): void;
     authorizeWithTemporaryUser(): AccessToken;
+    logout(): void;
 }
 
-export default React.createContext<UserContext>({
+const userContext = React.createContext<UserContext>({
     isAuthenticated: () => {
         throw Error("Not implemented");
     },
@@ -17,10 +18,17 @@ export default React.createContext<UserContext>({
         throw Error("Not implemented");
     },
     setUser: () => {
-        throw Error("Not implemented")
+        throw Error("Not implemented");
     },
     authorizeWithTemporaryUser: () => {
-        throw Error("Not implemented")
+        throw Error("Not implemented");
+    },
+    logout: () => {
+        throw Error("Not implemented");
     }
+});
 
-})
+export const UserContextProviderComponent = userContext.Provider;
+export const UserContextConsumer = userContext.Consumer;
+
+export default userContext;
