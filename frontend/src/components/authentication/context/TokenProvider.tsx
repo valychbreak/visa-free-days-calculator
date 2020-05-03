523b42f2211848e0a53180b95aea37b0e7f428ec
valychbreak <valychbreak@gmail.com>
2020-05-03 03:17:07 +0200

    [S19] Simplify null-safety check

--- frontend/src/components/authentication/context/TokenProvider.tsx
+++ frontend/src/components/authentication/context/TokenProvider.tsx
@@ -70,7 +70,7 @@ export class TokenProvider {
         const jwt = JSON.parse(atob(jwtToken.split('.')[1]));
     
         // multiply by 1000 to convert seconds into milliseconds
-        return jwt && jwt.exp && jwt.exp * 1000 || null;
+        return jwt?.exp ? jwt.exp * 1000 : null;
     };
 }
 
