6916d914b89815c2349ec24f5862c22bb61d12be
valychbreak <valychbreak@gmail.com>
2020-05-03 19:25:28 +0200

    [S8] Code refactoring

--- backend/src/main/java/com/valychbreak/calculator/service/authentication/UserCredentialsAuthenticationProvider.java
+++ backend/src/main/java/com/valychbreak/calculator/service/authentication/UserCredentialsAuthenticationProvider.java
@@ -1,33 +1,35 @@
 package com.valychbreak.calculator.service.authentication;
 
-import com.valychbreak.calculator.repository.UserRepository;
+import com.valychbreak.calculator.service.user.UserService;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.security.authentication.*;
-import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.Collections;
 
 import static io.micronaut.security.authentication.AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH;
 
 @Singleton
-public class UserCredentialsAuthenticationProvider implements AuthenticationProvider {
+public class UserCredentialsAuthenticationProvider implements HttpAuthenticationProvider {
 
-    private UserRepository userRepository;
+    private static final AuthenticationFailed FAILED_AUTHENTICATION = new AuthenticationFailed(CREDENTIALS_DO_NOT_MATCH);
+
+    private final UserService userService;
 
     @Inject
-    public UserCredentialsAuthenticationProvider(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserCredentialsAuthenticationProvider(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
-    public Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
-        Optional<AuthenticationResponse> authenticationResponse = userRepository.findByUsername((String) authenticationRequest.getIdentity())
-                .filter(foundUser -> authenticationRequest.getSecret().equals(foundUser.getPassword()))
-                .flatMap(foundUser -> Optional.of(new UserDetails(foundUser.getUsername(), new ArrayList<>())));
+    public Publisher<AuthenticationResponse> authenticate(HttpRequest<?> request, AuthenticationRequest<?, ?> authenticationRequest) {
+        String username = (String) authenticationRequest.getIdentity();
+        String password = (String) authenticationRequest.getSecret();
 
-        return Flowable.just(authenticationResponse.orElse(new AuthenticationFailed(CREDENTIALS_DO_NOT_MATCH)));
+        return userService.findUserBy(username, password)
+                .map(user -> (AuthenticationResponse) new UserDetails(user.getUsername(), Collections.emptyList()))
+                .onErrorReturn(FAILED_AUTHENTICATION);
     }
 }
